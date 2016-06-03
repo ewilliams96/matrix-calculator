e156c056927390b6968aade6a529031982f3c8c1
Elizabeth Williams <elwilliams@cs.stonybrook.edu>
2016-06-03 03:42:01 -0400

    fixed width of rows to screen size, made some methods more general, stuff so far works for matrix A and B now

--- java/me/elizabeth_williams/matrix/MainActivity.java
+++ java/me/elizabeth_williams/matrix/MainActivity.java
@@ -1,12 +1,17 @@
 package me.elizabeth_williams.matrix;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,26 +25,50 @@ public class MainActivity extends AppCompatActivity {
     private EditText colA;
 
     // matrix B size controls
-    //TODO:
+    private Button setB;
+    private EditText rowB;
+    private EditText colB;
 
     //matrix layout components
     GridLayout matrixA;
     GridLayout matrixB;
-
+    ArrayList<EditText> cellsA;
+    ArrayList<EditText> cellsB;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get matrix A controls from resources
         setA = (Button) findViewById(R.id.buttonA);
         rowA = (EditText) findViewById(R.id.editRowsA);
         colA = (EditText) findViewById(R.id.editColsA);
         matrixA = (GridLayout) findViewById(R.id.matrixA);
+
+        //Get matrix B controls from resources
+        setB = (Button) findViewById(R.id.buttonB);
+        rowB = (EditText) findViewById(R.id.editRowsB);
+        colB = (EditText) findViewById(R.id.editColsB);
+        matrixB = (GridLayout) findViewById(R.id.matrixB);
+        setB = (Button) findViewById(R.id.buttonB);
+
+
+        // listeners for setting matrix size
         setA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               setMatrixSize(MATRIX_A);
+               setMatrixSize(MATRIX_A, Integer.valueOf(rowA.getText().toString()),
+                       Integer.valueOf(rowA.getText().toString()));
+            }
+        });
+
+        setB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setMatrixSize(MATRIX_B, Integer.valueOf(rowB.getText().toString()),
+                        Integer.valueOf(colB.getText().toString()));
             }
         });
 
@@ -49,34 +78,77 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Set matrix size. Generate/edit corresponding gridlayout and
      * edit text cells.
-     * @param - matrix (see above constants)
+     * @param m - matrix (see above constants)
+     * @param r - rows
+     * @param c - cols
      */
-    private void setMatrixSize(int matrix){
+    private void setMatrixSize(int m, int r, int c){
         int rows;
         int cols;
+        GridLayout matrix;
+        ArrayList<EditText> cells;
 
+        rows = r;
+        cols = c;
 
-        if(matrix == MATRIX_A){
-            rows = inputToInt(rowA.getText().toString());
-            cols = inputToInt(colA.getText().toString());
+        if(m == MATRIX_A){
+
+            matrix = matrixA;
+            cellsA = new ArrayList<EditText>();
+            cells = cellsA;
         }
         else{
-            //TODO: row B stuff
-            rows = inputToInt(rowA.getText().toString());
-            cols = inputToInt(colA.getText().toString());
+
+            matrix = matrixB;
+            cellsB = new ArrayList<EditText>();
+            cells = cellsB;
+
         }
 
         // check for valid size input
         if(rows == -1 || cols == -1){
-            matrixSizeInvalid();
+            errorToast(R.string.sizeInvalid);
             return;
         }
 
+        // clear grid elements
+        matrix.removeAllViews();
+
         //proceed to setting matrix sizes
+        matrix.setRowCount(rows);
+        matrix.setColumnCount(cols);
+        int numCells =  rows * cols;
+
+        // width of grid
+        int gridWidth = matrix.getWidth();
+
+        for(int i = 0; i < numCells; i++){
+            EditText e = new EditText(this);
+
+            // get number input only
+            e.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+            int colWidth = gridWidth/cols; // set min width of columns
+            e.setMinimumWidth(colWidth);
+
+            // hide keyboard since the focus change doesn't work as I'd like right now
+            e.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    InputMethodManager imm =  (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            });
 
 
+            cells.add(e); // store reference to each cell in the array
+            matrix.addView(e);
+
+        }
 
     }
+
+
 
     /**
      *
@@ -93,12 +165,14 @@ public class MainActivity extends AppCompatActivity {
         return result;
     }
 
-    private void matrixSizeInvalid(){
+    private void errorToast(String s){
         Toast toast = new Toast(this);
-        toast.setText("Invalid matrix dimensions entered.");
+        toast.setText(s);
         toast.setDuration(Toast.LENGTH_SHORT);
 
     }
+
+
 
 
 
